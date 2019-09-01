f7d57f2c7e762714f746c51567bc96630eada0dd
Gilbert <gilbertlucas46@gmail.com>
2019-09-01 10:07:25 +0800

    Added AirBNB Strict ESLint file :hotel:

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "settings": {
+        "ecmascript": 6,
+        "jsx": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 2017,
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "experimentalDecorators": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+    ],
+    "extends": "airbnb",
+    "rules": {
+        "react/jsx-filename-extension": 0,
+        "function-paren-newline": 0,
+        "max-len": 0,
+        "react/jsx-tag-spacing": 0,
+        "react/self-closing-comp": 0,
+        "react/void-dom-elements-no-children": 0,
+        "react/no-unused-state": 0,
+        "react/prop-types": 0,
+        "jsx-a11y/anchor-is-valid": 0,
+        "no-console": 0
+    }
+};
